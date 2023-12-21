8f2b85dc57808a44208c87630651b412ba6483d3
AleksandrTrofimov411 <133000343+AleksandrTrofimov411@users.noreply.github.com>
2023-12-22 00:03:50 +1000

    Add Words and Categories factories

---
+++ database/factories/WordsFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Words;
+use App\Services\Word\Service;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\=Words>
+ */
+class WordsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+
+    protected $model = Words::class;
+
+    public function definition(): array
+    {
+        $word = $this->faker->word;
+        return [
+            "word" => $word,
+            "context" => $this->faker->text,
+            "translate" => 'Слово',
+            "category_id" => Service::getCategoryId($word)
+        ];
+    }
+}
